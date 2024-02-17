5bdabe497c664e27bdf51a4fa7e2022400fa9957
Mikhail Volkov <mk.volkov34@gmail.com>
2024-02-17 09:39:05 +0300

    map demo

--- main.go
+++ main.go
@@ -28,4 +28,12 @@ func main() {
 	fmt.Println("size of sl1 = ", unsafe.Sizeof(sl1))
 	fmt.Println("length of sl1 = ", len(sl1))
 	fmt.Println("capacity of sl1 = ", cap(sl1))
+
+	mp1 := make(map[string]int)
+	mp1["Arnold Schwarzenegger"] = len("Arnold Schwarzenegger")
+	mp1["Max Payne"] = len("Max Payne")
+	mp1["Daniil Dankovsky"] = len("Daniil Dankovsky")
+
+	fmt.Println("size of mp1 = ", unsafe.Sizeof(mp1))
+	fmt.Println("length of mp1 = ", len(mp1))
 }
